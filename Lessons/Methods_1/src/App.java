7387b5663a3cfe22f0d1a39af3770bc42278c863
Dilshan <tharakadilshan506@gmail.com>
2024-12-22 23:32:54 +0530

    app class

---
+++ Lessons/Methods_1/src/App.java
@@ -0,0 +1,11 @@
+
+public class App {
+
+	public static void main(String[] args) {
+
+		Person personA = new Person(); //object1, reference1
+		personA.setPersonName("FoS");
+		System.out.println(personA.name);
+	}
+
+}
