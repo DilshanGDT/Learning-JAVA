06a98a1e2ba36fdda4168b4f7f4f964066256d3a
Dilshan <tharakadilshan506@gmail.com>
2025-01-03 22:52:46 +0530

    add CarImpl class implementing CarInterface with basic methods

---
+++ Projects/Project_Fleet/src/CarImpl.java
@@ -0,0 +1,20 @@
+
+public class CarImpl implements CarInterface{
+
+	@Override
+	public void speedUp() {
+		
+	}
+
+	@Override
+	public void speedDown() {
+		
+	}
+
+	@Override
+	public void startEngine() {
+		
+	}
+
+	
+}
