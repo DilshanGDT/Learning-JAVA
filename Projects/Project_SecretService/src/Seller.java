2547fee72028fe93289dd77ce8b0b46a3db7253e
Dilshan <tharakadilshan506@gmail.com>
2025-01-16 21:00:01 +0530

    refactor Seller class to use final variables

--- Projects/Project_SecretService/src/Seller.java
+++ Projects/Project_SecretService/src/Seller.java
@@ -2,51 +2,59 @@ import java.util.Scanner;
 
 public class Seller {
 
-	private static String code = "RAPTOR";
+	private static final String code = "RAPTOR";
 	
-	private static Weapon bomb = new Bomb("Clemo", 12);
-	private static Weapon missile = new Missile("001", "Intercontinental", "Nuclear");
+	private static final Weapon bomb = new Bomb("Clemo", 12);
+	private static final Weapon missile = new Missile("001", "Intercontinental", "Nuclear");
 	
 	public static void sellWeapon(Terrorist trst, Agent agnt) {
-		Scanner scan = new Scanner(System.in);
-		
-		System.out.println("Enter the Password : ");
-		String password = scan.nextLine();
-		
-		if (password.equals(code)) {
-			System.out.println("Password is Correct!");
-			System.out.println("Welcome to the Store \n");
-			System.out.println("Choose a weapon : ");
-			System.out.println("Enter 1 for Missile & 2 for Bomb : ");
+		try (Scanner scan = new Scanner(System.in)) {
+			System.out.println("Enter the Password : ");
+			String password = scan.nextLine();
 			
-			int weaponNo = scan.nextInt();
-			
-			switch (weaponNo) {
-			
-			case 1:
-				trst.buyWeapon(missile);
-				agnt.catchTerrorist(trst);
-				agnt.promote("Major");
-				agnt.addExperience();
-				agnt.missionSuccess();
-				System.out.println(agnt.codeName + " Promoted to " + agnt.printCurrentRank());
-				break;
+			if (password.equals(code)) {
+				System.out.println("Password is Correct!");
+				System.out.println("Welcome to the Store \n");
+				System.out.println("Choose a weapon : ");
+				System.out.println("Enter 1 for Missile & 2 for Bomb : ");
 				
-			case 2:
-				trst.buyWeapon(bomb);
-				trst.attack();
-				break;
+				int weaponNo = scan.nextInt();
 				
-			default :
-				break;
+				switch (weaponNo) {
 				
+				case 1 -> {
+                                    trst.buyWeapon(missile);
+                                    agnt.catchTerrorist(trst);
+                                    agnt.promote("Major");
+                                    agnt.addExperience();
+                                    agnt.missionSuccess();
+                                    System.out.println(agnt.codeName + " Promoted to " + agnt.printCurrentRank());
+                                }
+					
+				case 2 -> {
+                                    trst.buyWeapon(bomb);
+                                    trst.attack();
+                                }
+					
+				default -> {
+                                }
+					
+				}
+				
+			} else {
+				System.out.println("Password is Incorrect!");
+				System.out.println("System is blocked for the user");
 			}
-			
-		} else {
-			System.out.println("Password is Incorrect!");
-			System.out.println("System is blocked for the user");
 		}
 		
 	}
+
+    public static String getCode() {
+        return code;
+    }
+
+    public static Weapon getMissile() {
+        return missile;
+    }
 	
 }
